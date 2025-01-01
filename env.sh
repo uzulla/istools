257d7ee5070e0c40754cb47785d59acb8d37543f
uzulla <uzulla@himitsukichi.com>
2025-01-01 13:13:24 +0900

    add env.sh

---
+++ env.sh
@@ -0,0 +1,7 @@
+export PREBUILD_S3_BUCKET=yourbucket
+export PREBUILD_AWS_REGION=ap-northeast-1
+export PREBUILD_DISCORD_WEBHOOK_URL=https://discord.com/api/webhooks/123/1234
+export PREBUILD_AWS_ACCESS_KEY_ID=AKIA***
+export PREBUILD_AWS_SECRET_ACCESS_KEY=***
+export PREBUILD_GITHUB_TOKEN=ghp_***
+export PREBUILD_GITHUB_ISSUE_URL=https://github.com/***/***/issue/1
